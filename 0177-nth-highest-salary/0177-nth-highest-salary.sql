aea93ee13345e5060624bba2ed545ae72b96356b
Yeshaswini R <yeshaswini.r.123@gmail.com>
2026-09-05 20:44:17 +0530

    Time: 427 ms (90.99%), Space: 0B (100%) - LeetHub

--- 0177-nth-highest-salary/0177-nth-highest-salary.sql
+++ 0177-nth-highest-salary/0177-nth-highest-salary.sql
@@ -1,10 +1,12 @@
 CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
 BEGIN
-    SET N = N - 1;
   RETURN (
-      SELECT DISTINCT salary
-        FROM Employee
-        ORDER BY salary DESC
-        LIMIT 1 OFFSET N
+      SELECT salary FROM
+        (SELECT 
+        salary,
+        DENSE_RANK() OVER(ORDER BY salary DESC) AS rnk
+        FROM Employee) AS t
+    WHERE rnk = N
+    LIMIT 1
     );
 END
